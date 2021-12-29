1506ec8fb845627d0dc45145908117e958666a84
TWoolff <92778181+TWoolff@users.noreply.github.com>
2021-12-29 16:15:52 +0100

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <transition name="fade">
-    <section>
-      <div v-if="stories.length >= 10" class="grid">
-        <article v-for="story in stories" :key="story" class="story">
-          <span></span>
-          <h2><a :href='story.data.url'>{{ story.data.title }}</a></h2>
-          <p>Timestamp: {{ story.data.time }}</p>
-          <p>Story Score: {{ story.data.score }}</p>
-          <br>
-          <p>Author: {{ story.data.by }}</p>
-          <p>Karma Score: {{ story.data.user.data.karma }}</p>
-          <div class="story-image" :style="{backgroundImage: `url(${images[Math.floor(Math.random() * images.length)]})`}"></div>
-        </article>
-      </div>
-      <div v-else>
-        <Spinner />
-      </div>
-    </section>
-  </transition>
+  <section>
+    <div v-if="stories.length >= 10" class="grid">
+      <article v-for="story in stories" :key="story" class="story">
+        <span></span>
+        <h2><a :href='story.data.url'>{{ story.data.title }}</a></h2>
+        <p>Timestamp: {{ story.data.time }}</p>
+        <p>Story Score: {{ story.data.score }}</p>
+        <br>
+        <p>Author: {{ story.data.by }}</p>
+        <p>Karma Score: {{ story.data.user.data.karma }}</p>
+        <div class="story-image" :style="{backgroundImage: `url(${images[Math.floor(Math.random() * images.length)]})`}"></div>
+      </article>
+    </div>
+    <div v-else>
+      <Spinner />
+    </div>
+  </section>
 </template>
 
 <script>
--- src/composables/getStories.js
+++ src/composables/getStories.js
@@ -4,33 +4,21 @@ import axios from 'axios'
 const getStories = () => {
   const baseUrl = 'https://hacker-news.firebaseio.com/v0'
   const stories = ref([])
-  const images = ref([
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg',
-  '/img/11.jpg',
-  '/img/12.jpg'])
+  const images = ref(['/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg', '/img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg', '/img/10.jpg', '/img/11.jpg', '/img/12.jpg'])
 
   onMounted(async () => {
     const storyIds = await axios.get(`${baseUrl}/topstories.json`)
-    let indexes = [];
+    const arr = [];
 
-    while (indexes.length < 10) { 
-      const x = Math.floor(Math.random() * storyIds.data.length)
-      if (indexes.indexOf(x) < 0) {
-        indexes.push(x);
+    while (arr.length < 10) { 
+      const randomItem = Math.floor(Math.random() * storyIds.data.length)
+      if (arr.indexOf(randomItem) < 0) {
+        arr.push(randomItem);
       }
     }
 
-    for (let i = 0; i < indexes.length; i++) {
-      const storyId = storyIds.data[indexes[i]]
+    for (let i = 0; i < arr.length; i++) {
+      const storyId = storyIds.data[arr[i]]
       const story = await axios.get(`${baseUrl}/item/${storyId}.json`)
       story.data.user = await axios.get(`${baseUrl}/user/${story.data.by}.json`)
       stories.value.push(story)
