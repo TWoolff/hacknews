d55679d14e0acf745cb1338ec285a85c4b0a81b7
TWoolff <92778181+TWoolff@users.noreply.github.com>
2021-12-30 12:24:01 +0100

    Date added

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
         <span></span>
         <h2><a :href='story.data.url'>{{ story.data.title }}</a></h2>
         <p>Timestamp: {{ story.data.time }}</p>
+        <p>Date: {{ new Date(story.data.time * 1000).toLocaleDateString("en-UK") }}</p>
         <p>Story Score: {{ story.data.score }}</p>
-        <br>
         <p>Author: {{ story.data.by }}</p>
         <p>Karma Score: {{ story.data.user.data.karma }}</p>
         <div class="story-image" :style="{backgroundImage: `url(${images[Math.floor(Math.random() * images.length)]})`}"></div>
